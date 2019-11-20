67952c5b2b1e9f53b3f3b270d859bf03f503fcae
배희수 <huisu0715@khu.ac.kr>
2019-11-20 13:33:44 +0900

    mybatis, mysql, jsp linking

--- src/main/java/com/example/demo/BoardApplication.java
+++ src/main/java/com/example/demo/BoardApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@MapperScan(value={"com.example.demo.board.mapper"})
 public class BoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+	
+	/*
+	 * SqlSessionFactoy Configuration
+	 */
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		
+		sessionFactory.setDataSource(dataSource);
+		return sessionFactory.getObject();
 	}
 
 }
---
+++ src/main/java/com/example/demo/board/controller/BoardController.java
@@ -0,0 +1,6 @@
+package com.example.demo.board.controller;
+
+
+public class BoardController {
+
+}
